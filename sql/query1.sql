071ae15aee2fbbf5e9bb81a776f1f24d1b47ced3
Enis Afgan <afgane@gmail.com>
2021-07-07 19:57:46 -0400

    Simplify date formatting

--- sql/query1.sql
+++ sql/query1.sql
@@ -1,7 +1,6 @@
 /* Number of jobs per tool per month (ie, replicate this diagram) */
 
-select tool_id,concat(cast(EXTRACT(year FROM cast(job.create_time as date)) as varchar(4)),'-',cast(EXTRACT(month FROM cast(job.create_time as date)) as varchar(2))) as month_year,count(DISTINCT job_id) as num_jobs
+select to_char(job.create_time, 'YYYY-MM') as date, tool_id, count(DISTINCT job_id) as num_jobs
 from job join job_metric_numeric on job.id = job_metric_numeric.job_id
-group by tool_id,month_year
-order by month_year asc,num_jobs desc;
-
+group by tool_id, date
+order by date asc, num_jobs desc;
