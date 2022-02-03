5709c87734c0fbcce409988dfae08e34d5222721
Keith Suderman <suderman@jhu.edu>
2022-02-03 11:33:14 -0500

    Add ansible playbooks and helm configurations

--- query-all.sh
+++ query-all.sh
@@ -3,18 +3,15 @@
 #QUERIES="numjobs history_runtimes"
 QUERIES="all_history_runtimes"
 
-if [[ ! -e $1.sh ]] ; then
-	echo "ERROR: No such configuration. Should be one of jetstream.sh, aws.sh, or gcp.sh"
-	exit 1
+if [[ ! -e results ]] ; then
+	echo "Making output directory"
+	mkdir results
 fi
 
-source ./$1.sh
-
-if [[ ! -e $KUBECONFIG ]] ; then
-	echo "ERROR: kubeconfig $KUBECONFIG not found."
-	exit 1
-fi
-
-for query in $QUERIES ; do
-	kubectl exec -in $NAMESPACE $POD -- sudo -u postgres psql -d galaxy --csv < sql/$query.sql > results/$1-$query.csv
+for cloud in js aws gcp ; do
+  source ./$cloud.sh
+  for query in $@ ; do
+    echo "Running $query on $cloud"
+	  kubectl exec -in $NAMESPACE $POD -- sudo -u postgres psql -d galaxy --csv < sql/$query.sql > results/$cloud-$query.csv
+	done
 done
