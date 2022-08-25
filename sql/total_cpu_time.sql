998a1745bafd7d21417c966464ab6cdcf95b3da3
hujambo-dunia <michelle.savage.1@gmail.com>
2022-08-25 09:12:57 -0400

    12 processed data files; 1 sql instructions file

---
+++ sql/total_cpu_time.sql
@@ -0,0 +1,24 @@
+-- Total amount of cpu time per tool per month, in seconds.
+
+WITH cpu_usage AS (
+    SELECT
+        DISTINCT job_id,
+        metric_value / 1000000000 AS cpu_usage_seconds  -- Convert from nanoseconds to seconds.
+    FROM
+        job_metric_numeric
+    WHERE
+        metric_name = 'cpuacct.usage'
+)
+SELECT
+    TO_CHAR(job.create_time, 'YYYY-MM') AS date,
+    job.tool_id,
+    ROUND(SUM(cpu_usage.cpu_usage_seconds), 0) AS total_cpu_time_seconds
+FROM
+    job
+    JOIN cpu_usage ON job.id = cpu_usage.job_id
+GROUP BY
+    date,
+    job.tool_id
+ORDER BY
+    date ASC,
+    total_cpu_time_seconds DESC;
